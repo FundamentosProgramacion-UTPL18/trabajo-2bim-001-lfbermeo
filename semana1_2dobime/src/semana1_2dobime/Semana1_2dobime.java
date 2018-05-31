822a6a4ddccb75b08fcab1ff0dfc3f1986aca5e8
Luisa Bermeo <lfbermeo>
2018-05-31 11:34:14 -0500

    Actualizando trabajo

--- semana1_2dobime/src/semana1_2dobime/Semana1_2dobime.java
+++ semana1_2dobime/src/semana1_2dobime/Semana1_2dobime.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package semana1_2dobime;
 
-/**
- *
- * @author DELL
- */
+import java.util.Scanner;
+
 public class Semana1_2dobime {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       Scanner entrada = new Scanner(System.in);
+        int [] c = new int [4];
+        c[0]=10;
+        
+        for (int contador=0 ; contador < c.length ; contador++){
+            System.out.println(c[contador]);
+        }
     }
     
 }
